f0a8c4d74383c67faa544659111fc6b702db9c9b
Sebastian Ladino <sebasladino0@gmail.com>
2024-09-07 20:35:26 -0500

    Ya termine el protafolio por el momento, a futuro subir más proyectos, adjuntar los links de las redes, hacerlo un poco más dinamico, tuve complciaciones pero supe solucionar los errores, falta subirlo a enyoi y finalizo

---
+++ script.js
@@ -0,0 +1,39 @@
+let menuVisible = false;
+/* Función que muestra u oculta el menu */
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+function seleccionar(){
+    /* Ocultar el menu una vez que se seleccione */
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//función animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("python");
+        habilidades[3].classList.add("cs");
+        habilidades[4].classList.add("unity");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+//Dectecto el scroll para aplicar las animaciones de las habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
